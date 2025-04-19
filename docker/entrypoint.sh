4a3500ecb31be68eae127798dae3b83f5b90a709
Hassan Ahmad Usmani <ihassanusmani@gmail.com>
2025-04-19 06:43:04 +0500

    entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $# -eq 1 ]]; then
-    echo "Processing Job From queue $1"
+    echo "Processing Job From queue - 1-> $1 - 2->  $2"
     php artisan queue:work sqs --queue=$1 --timeout=100000 --once
 elif [[ $# -eq 2 ]]; then
     echo "Processing Job From queue $1 driver $2"
@@ -8,3 +8,5 @@ elif [[ $# -eq 2 ]]; then
 else
     echo "No Job Queue Provided to Process Job"
 fi
+
+#php artisan queue:work sqs --queue=sp-report-request-api --timeout=100000
